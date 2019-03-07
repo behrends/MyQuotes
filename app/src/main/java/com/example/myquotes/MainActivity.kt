1f484efeb35d4e648627ab91a22f7adc3f79d523
Erik Behrends <behrends@users.noreply.github.com>
2019-03-07 18:29:02 +0100

    Button zum Blättern hinzufügen

--- app/src/main/java/com/example/myquotes/MainActivity.kt
+++ app/src/main/java/com/example/myquotes/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.myquotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun nextQuote(view: View) {
+        Toast.makeText(this, "Nächstes Zitat", Toast.LENGTH_SHORT).show()
     }
 }
