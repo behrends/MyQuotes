b2ad831b86a37e1c80426876ebfed7fa8bda5508
Erik Behrends <behrends@users.noreply.github.com>
2019-03-20 12:28:06 +0100

    Zitat im UI aus der Activity setzen

--- app/src/main/java/com/example/myquotes/MainActivity.kt
+++ app/src/main/java/com/example/myquotes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myquotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.quote_text).text = "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind."
+        findViewById<TextView>(R.id.quote_author).text = "Albert Einstein"
+        findViewById<TextView>(R.id.quote_year).text = "1948"
     }
 
     fun nextQuote(view: View) {
