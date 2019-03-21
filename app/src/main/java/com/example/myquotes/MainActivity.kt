5a79fd395896f9dbaeeb62146b7a6948dd220d15
Erik Behrends <behrends@users.noreply.github.com>
2019-03-21 21:39:14 +0100

    Button für nächstes Zitat implementieren

--- app/src/main/java/com/example/myquotes/MainActivity.kt
+++ app/src/main/java/com/example/myquotes/MainActivity.kt
@@ -8,23 +8,40 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private val quotes = listOf(
+        Quote(
+            "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
+            "Albert Einstein",
+            "1948"
+        ),
+        Quote(
+            "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
+            "Marie Curie",
+            "1928"
+        ),
+        Quote(
+            "Nichts ist so beständig wie der Wandel.",
+            "Heraklit",
+            "480 v. Chr."
+        )
+    )
+
+    private var index = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val quote = Quote(
-            "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
-            "Albert Einstein",
-            "1948"
-        )
-
-        findViewById<TextView>(R.id.quote_text).text = quote.text
-        findViewById<TextView>(R.id.quote_author).text = quote.author
-        findViewById<TextView>(R.id.quote_year).text = quote.year
+        findViewById<TextView>(R.id.quote_text).text = quotes[index].text
+        findViewById<TextView>(R.id.quote_author).text = quotes[index].author
+        findViewById<TextView>(R.id.quote_year).text = quotes[index].year
     }
 
     fun nextQuote(view: View) {
-        Toast.makeText(this, "Nächstes Zitat", Toast.LENGTH_SHORT).show()
+        if(index < quotes.size - 1) index++
+        findViewById<TextView>(R.id.quote_text).text = quotes[index].text
+        findViewById<TextView>(R.id.quote_author).text = quotes[index].author
+        findViewById<TextView>(R.id.quote_year).text = quotes[index].year
     }
 
     fun previousQuote(view: View) {
