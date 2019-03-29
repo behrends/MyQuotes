82a474297ae308ee8a9c20091685309a3c9f5d8b
Erik Behrends <behrends@users.noreply.github.com>
2019-03-29 17:43:13 +0100

    Durch ViewModel die UI-Logik aus der Activity herauslösen

--- app/src/main/java/com/example/myquotes/MainActivity.kt
+++ app/src/main/java/com/example/myquotes/MainActivity.kt
@@ -2,70 +2,37 @@ package com.example.myquotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.myquotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var model: QuoteViewModel
-
-    private val quotes = listOf(
-        Quote(
-            "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
-            "Albert Einstein",
-            "1948"
-        ),
-        Quote(
-            "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
-            "Marie Curie",
-            "1928"
-        ),
-        Quote(
-            "Nichts ist so beständig wie der Wandel.",
-            "Heraklit",
-            "480 v. Chr."
-        )
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        model = ViewModelProviders.of(this).get(QuoteViewModel::class.java)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewmodel = ViewModelProviders.of(this).get(QuoteViewModel::class.java)
 
-        showQuote()
-        setVisibilityOfButtons()
+//        showQuote()
+//        setVisibilityOfButtons()
     }
 
-    private fun showQuote() {
-        binding.quote = quotes[model.index]
-    }
-
-    private fun setVisibilityOfButtons() = when (model.index) {
-        0 -> binding.apply {
-            quotePrevious.visibility = View.INVISIBLE
-            quoteNext.visibility = if(quotes.size > 1) View.VISIBLE else View.INVISIBLE
-        }
-        quotes.size - 1 -> binding.apply {
-            quoteNext.visibility = View.INVISIBLE
-            quotePrevious.visibility = if(quotes.size > 1) View.VISIBLE else View.INVISIBLE
-        }
-        else -> binding.apply {
-            quotePrevious.visibility = View.VISIBLE
-            quoteNext.visibility = View.VISIBLE
-        }
-    }
-
-    fun nextQuote(view: View) {
-        if(model.index < quotes.size - 1) model.index++
-        showQuote()
-        setVisibilityOfButtons()
-    }
-
-    fun previousQuote(view: View) {
-        if(model.index > 0) model.index--
-        showQuote()
-        setVisibilityOfButtons()
-    }
+//    private fun showQuote() {
+//        binding.quote = quotes[model.index]
+//    }
+//
+//    private fun setVisibilityOfButtons() = when (model.index) {
+//        0 -> binding.apply {
+//            quotePrevious.visibility = View.INVISIBLE
+//            quoteNext.visibility = if(quotes.size > 1) View.VISIBLE else View.INVISIBLE
+//        }
+//        quotes.size - 1 -> binding.apply {
+//            quoteNext.visibility = View.INVISIBLE
+//            quotePrevious.visibility = if(quotes.size > 1) View.VISIBLE else View.INVISIBLE
+//        }
+//        else -> binding.apply {
+//            quotePrevious.visibility = View.VISIBLE
+//            quoteNext.visibility = View.VISIBLE
+//        }
+//    }
 }
--- app/src/main/java/com/example/myquotes/QuoteViewModel.kt
+++ app/src/main/java/com/example/myquotes/QuoteViewModel.kt
@@ -1,7 +1,41 @@
 package com.example.myquotes
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 
 class QuoteViewModel : ViewModel() {
+    private val quotes = listOf(
+        Quote(
+            "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
+            "Albert Einstein",
+            "1948"
+        ),
+        Quote(
+            "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
+            "Marie Curie",
+            "1928"
+        ),
+        Quote(
+            "Nichts ist so beständig wie der Wandel.",
+            "Heraklit",
+            "480 v. Chr."
+        )
+    )
+
     var index = 0
+    var quote = ObservableField(quotes[index])
+
+    fun nextQuote() {
+        if(index < quotes.size - 1) {
+            index++
+            quote.set(quotes[index])
+        }
+    }
+
+    fun previousQuote() {
+        if(index > 0) {
+            index--
+            quote.set(quotes[index])
+        }
+    }
 }
