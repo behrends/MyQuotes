a2474b49662b2ad8279e4f6b3558adb3dd64cd5a
Erik Behrends <behrends@users.noreply.github.com>
2019-07-11 18:25:17 +0200

    Automatisch zum neuen Zitat blättern

--- app/src/main/java/com/example/myquotes/MainActivity.kt
+++ app/src/main/java/com/example/myquotes/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myquotes.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.dialog_new_quote.*
@@ -22,8 +23,15 @@ class MainActivity : AppCompatActivity() {
 
         val viewPager = binding.quoteViewPager
         val adapter = QuoteViewPagerAdapter()
-        adapter.setQuotes(viewModel.quotes)
         viewPager.adapter = adapter
+
+        viewModel.quotes.observe(this, Observer { quotes ->
+            adapter.setQuotes(quotes)
+            if(viewModel.newQuoteAdded) {
+                viewPager.setCurrentItem(quotes.size - 1, true)
+                viewModel.newQuoteAdded = false
+            }
+        })
     }
 
     fun addQuote(view: View) {
--- app/src/main/java/com/example/myquotes/QuoteViewModel.kt
+++ app/src/main/java/com/example/myquotes/QuoteViewModel.kt
@@ -1,28 +1,40 @@
 package com.example.myquotes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class QuoteViewModel : ViewModel() {
-    val quotes = mutableListOf(
-        Quote(
-            "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
-            "Albert Einstein",
-            "1948"
-        ),
-        Quote(
-            "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
-            "Marie Curie",
-            "1928"
-        ),
-        Quote(
-            "Nichts ist so beständig wie der Wandel.",
-            "Heraklit",
-            "480 v. Chr."
+    private val _quotes = MutableLiveData<MutableList<Quote>>().apply {
+        value = mutableListOf(
+            Quote(
+                "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
+                "Albert Einstein",
+                "1948"
+            ),
+            Quote(
+                "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
+                "Marie Curie",
+                "1928"
+            ),
+            Quote(
+                "Nichts ist so beständig wie der Wandel.",
+                "Heraklit",
+                "480 v. Chr."
+            )
         )
-    )
+    }
+
+    val quotes: LiveData<MutableList<Quote>>
+        get() = _quotes
+
+    var newQuoteAdded = false
 
     fun createQuote(text: String, author: String, year: String) {
+        newQuoteAdded = true
         val quote = Quote(text, author, year)
-        quotes.add(quote)
+        val list = _quotes.value ?: mutableListOf()
+        list.add(quote)
+        _quotes.value = list
     }
 }
