b9d3f37a5564de0be57e6dc62197d9440ac7d4a0
Erik Behrends <behrends@users.noreply.github.com>
2019-07-12 10:38:28 +0200

    Mit BindingAdapter einen Hinweis bei leerer Zitatenliste anzeigen

--- app/src/main/java/com/example/myquotes/QuoteBindingAdapters.kt
+++ app/src/main/java/com/example/myquotes/QuoteBindingAdapters.kt
@@ -3,7 +3,7 @@ package com.example.myquotes
 import android.view.View
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("hideView")
-fun hideView(view: View, hide: Boolean) {
-    view.visibility = if (hide) View.INVISIBLE else View.VISIBLE
+@BindingAdapter("isGone")
+fun isGone(view: View, gone: Boolean) {
+    view.visibility = if (gone) View.GONE else View.VISIBLE
 }
--- app/src/main/java/com/example/myquotes/QuoteViewModel.kt
+++ app/src/main/java/com/example/myquotes/QuoteViewModel.kt
@@ -2,31 +2,18 @@ package com.example.myquotes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class QuoteViewModel : ViewModel() {
     private val _quotes = MutableLiveData<MutableList<Quote>>().apply {
-        value = mutableListOf(
-            Quote(
-                "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
-                "Albert Einstein",
-                "1948"
-            ),
-            Quote(
-                "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
-                "Marie Curie",
-                "1928"
-            ),
-            Quote(
-                "Nichts ist so beständig wie der Wandel.",
-                "Heraklit",
-                "480 v. Chr."
-            )
-        )
+        value = mutableListOf()
     }
 
     val quotes: LiveData<MutableList<Quote>>
         get() = _quotes
+
+    val hasNoQuotes = Transformations.map(quotes) { quotes.value.isNullOrEmpty() }
 
     var newQuoteAdded = false
 
